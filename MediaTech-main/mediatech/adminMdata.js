af3ff520e4648ba858c617f7069391ea27797bd6
Salik Bassam <117837220+SalikBassam@users.noreply.github.com>
2023-03-24 13:25:20 +0100

    Delete MediaTech-main/mediatech directory

--- MediaTech-main/mediatech/adminMdata.js
+++
@@ -1,9 +0,0 @@
-let trash = document.querySelectorAll(".trash")
-let modal = document.querySelector(".modal-body")
-
-trash.forEach(e => {
-e.addEventListener("click",function(){
-    modal.innertext+="teste"
-})
-});
-
--- MediaTech-main/mediatech/dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dsn = 'mysql:host=localhost;dbname=mediatech';
-$username = 'root';
-$password = '';
-
-
-  $pdo = new PDO($dsn, $username, $password);
-?>
