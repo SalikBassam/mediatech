1f2003d05f82f1b7eec6df2f02ef7e9f307a488b
Salik Bassam <117837220+SalikBassam@users.noreply.github.com>
2023-03-27 02:16:06 +0000

    Add files via upload

---
+++ mediatech/js/adminMdata.js
@@ -0,0 +1,10 @@
+// const modal = document.querySelector('#exampleModal');
+// modal.addEventListener('show.bs.modal', function(event) {
+//   const button = event.relatedTarget;
+//   const productId = button.getAttribute('data-product_id');
+//   const productName = button.closest('.product').querySelector('p:first-child').textContent;
+//   const deleteProductId = modal.querySelector('#delete-product-id');
+//   const productNameSpan = modal.querySelector('#product-name');
+//   deleteProductId.value = productId;
+//   productNameSpan.textContent = productName;
+// });
